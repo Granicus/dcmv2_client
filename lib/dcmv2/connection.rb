b56a1ac54f25f4be3e1fad96c3660cb3048284b9
Bill Bushey <bill.bushey@govdelivery.com>
2016-01-12 14:27:30 -0600

    Throw a DCMv2::Suppressed if when conflict happens

--- lib/dcmv2/connection.rb
+++ lib/dcmv2/connection.rb
@@ -23,6 +23,8 @@ class DCMv2::Connection
       raise DCMv2::Unauthorized, "Unauthorized response. Please double check your API key."
     when Net::HTTPNotFound
       raise DCMv2::NotFound, "Could not find the requested resource."
+    when Net::HTTPConflict
+      raise DCMv2::Suppressed, "Email Address or Phone Number has been suppressed. Can not subscribe or message."
     else
       raise StandardError, "An error occurred when connecting to the server. #{response.response.class if response.response}"
     end
@@ -44,7 +46,8 @@ class DCMv2::Connection
   end
 end
 
-class DCMv2::Unauthorized < StandardError; end
-class DCMv2::NotFound < StandardError; end
 class DCMv2::InvalidResource < StandardError; end
+class DCMv2::NotFound < StandardError; end
+class DCMv2::Suppressed < StandardError; end
+class DCMv2::Unauthorized < StandardError; end
 
