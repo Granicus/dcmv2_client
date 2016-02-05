48a5d797a576dc01bfb8075d1aaeb2d1453ba3a8
Alex Ives <alex.ives@code42.com>
2016-02-05 13:02:09 -0600

    Added handling for response code 422 and added tests for Connection.make_request

--- spec/lib/dcmv2/connection_spec.rb
+++ spec/lib/dcmv2/connection_spec.rb
@@ -16,6 +16,73 @@ describe DCMv2::Connection do
     connection.api_key.should == '42'
   end
 
+  context "when making a request" do
+    let(:connection) { DCMv2::Connection.new(api_key) }
+
+    it "returns a parsed response for HTTPOK", active:true do
+      response = double("response")
+      allow(response).to receive(:response).and_return(Net::HTTPOK.new(0,200,""))
+      allow(response).to receive(:parsed_response).and_return("OKAY RESPONSE")
+      expect(DCMv2::Connection).to receive(:get).and_return(response)
+      connection.make_request().should == "OKAY RESPONSE"
+    end
+
+    it "returns a parsed response for HTTPCreated", active:true do
+      response = double("response")
+      allow(response).to receive(:response).and_return(Net::HTTPCreated.new(0,201,""))
+      allow(response).to receive(:parsed_response).and_return("OKAY RESPONSE")
+      expect(DCMv2::Connection).to receive(:get).and_return(response)
+      connection.make_request().should == "OKAY RESPONSE"
+    end
+
+    it "returns a parsed response for HTTPAccepted", active:true do
+      response = double("response")
+      allow(response).to receive(:response).and_return(Net::HTTPAccepted.new(0,202,""))
+      allow(response).to receive(:parsed_response).and_return("OKAY RESPONSE")
+      expect(DCMv2::Connection).to receive(:get).and_return(response)
+      connection.make_request().should == "OKAY RESPONSE"
+    end
+
+    it "raises DCMv2 Unauthorized for HTTPUnauthorized", active:true do
+      response = double("response")
+      allow(response).to receive(:response).and_return(Net::HTTPUnauthorized.new(0,401,""))
+      expect(DCMv2::Connection).to receive(:get).and_return(response)
+      expect { connection.make_request() }.to raise_error(DCMv2::Unauthorized)
+    end
+
+    it "raises DCMv2 NotFound for HTTPNotFound", active:true do
+      response = double("response")
+      allow(response).to receive(:response).and_return(Net::HTTPNotFound.new(0,404,""))
+      expect(DCMv2::Connection).to receive(:get).and_return(response)
+      expect { connection.make_request() }.to raise_error(DCMv2::NotFound)
+    end
+
+    it "raises DCMv2 Suppressed for HTTPConflict", active:true do
+      response = double("response")
+      allow(response).to receive(:response).and_return(Net::HTTPConflict.new(0,409,""))
+      expect(DCMv2::Connection).to receive(:get).and_return(response)
+      expect { connection.make_request() }.to raise_error(DCMv2::Suppressed)
+    end
+
+    it "raises DCMv2 InvalidRequest for HTTPClientError 422", active:true do
+      response = double("response")
+      allow(response).to receive(:response).and_return(Net::HTTPClientError.new(0,422,""))
+      allow(response).to receive(:code).and_return(422)
+      allow(response).to receive(:parsed_response).and_return("ERROR: NOT FOUND!")
+      expect(DCMv2::Connection).to receive(:get).and_return(response)
+      expect { connection.make_request() }.to raise_error(DCMv2::InvalidRequest, "ERROR: NOT FOUND!")
+    end
+
+    it "raises DCMv2 ClientError for other HTTPClientError", active:true do
+      response = double("response")
+      allow(response).to receive(:response).and_return(Net::HTTPClientError.new(0,418,""))
+      allow(response).to receive(:code).and_return(418)
+      expect(DCMv2::Connection).to receive(:get).and_return(response)
+      expect { connection.make_request() }.to raise_error(DCMv2::ClientError)
+    end
+
+  end
+
   context "when building a url" do
     let(:connection) { DCMv2::Connection.new(api_key) }
 
